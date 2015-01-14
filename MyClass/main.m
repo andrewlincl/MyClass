580f7e6582d11687b3bd75778d3641010f47b531
andrewlincl <andrew.lincl@gmail.com>
2015-01-14 16:44:32 +0800

    Initial Commit

---
+++ MyClass/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MyClass
+//
+//  Created by Lin Andrew on 2015/1/14.
+//  Copyright (c) 2015年 Andrew Lin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
